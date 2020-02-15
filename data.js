5610fa3100b0bf5fca45e7d6afdcccaa13c7ea0f
Yu Bin Jee <yubinjee@Yus-MacBook-Pro.local>
2020-02-15 18:35:36 +0100

    started working in html

---
+++ data.js
@@ -0,0 +1,226 @@
+var data = {
+    "auction": {
+        "auction_status": "CLOSED",
+        "auction_type": "OPEN",
+        "created_at": "2020-01-27T16:02:18.761311+01:00",
+        "deadline": "2020-01-31T12:13:00+01:00",
+        "deal_type": "BUYING",
+        "id": 203,
+        "ref": "X48CDKVFPYHK",
+        "start_price": "12.0000"
+    },
+    
+    "order": {
+        "delivery_date": "2022-03",
+        "id": 229,
+        "labels": [
+            "T\u00dcV_S\u00dcD_EE01_&_EE02"
+        ],
+        "origins": [
+            "BE",
+            "CH",
+            "CY",
+            "CZ",
+            "DE",
+            "DK",
+            "EE",
+            "ES",
+            "FI",
+            "FR",
+            "GR",
+            "HR",
+            "IE",
+            "IS",
+            "IT",
+            "LT",
+            "LU",
+            "LV",
+            "NL",
+            "NO",
+            "RS",
+            "SE",
+            "SI",
+            "SK"
+        ],
+        "support": true,
+        "technologies": [
+            "HYDRO"
+        ],
+        "vintage": 2021,
+        "volume": "123123"
+    },
+
+    "offers": [
+        {
+            "id": 218,
+            "origins": [
+                "BE"
+            ],
+            "price": "23.0000",
+            "ref": "0FG9VSR4E5FL",
+            "technologies": [
+                "HYDRO"
+            ],
+            "volume": "23"
+        },
+        {
+            "id": 222,
+            "origins": [
+                "CY"
+            ],
+            "price": "666.0000",
+            "ref": "RWM88CPRI9H1",
+            "technologies": [
+                "HYDRO"
+            ],
+            "volume": "7676"
+        },
+        {
+            "id": 219,
+            "origins": [
+                "BE"
+            ],
+            "price": "3212.0000",
+            "ref": "140MBONPELWM",
+            "technologies": [
+                "HYDRO"
+            ],
+            "volume": "23"
+        },
+        {
+            "id": 221,
+            "origins": [
+                "BE"
+            ],
+            "price": "13232.0000",
+            "ref": "LPE2PRWUTW8Z",
+            "technologies": [
+                "HYDRO"
+            ],
+            "volume": "12"
+        },
+        {
+            "id": 220,
+            "origins": [
+                "BE"
+            ],
+            "price": "13232.0000",
+            "ref": "824TNR2I5MU9",
+            "technologies": [
+                "HYDRO"
+            ],
+            "volume": "12"
+        }
+    ],
+
+    "transactions": [
+        {
+            "auction_id": 203,
+            "id": 18,
+            "offer": {
+                "id": 218,
+                "origins": [
+                    "BE"
+                ],
+                "price": "23.0000",
+                "ref": "0FG9VSR4E5FL",
+                "technologies": [
+                    "HYDRO"
+                ],
+                "volume": "23"
+            },
+            "ref": "KEZX657VGANC",
+            "tx_volume": "23"
+        },
+        {
+            "auction_id": 203,
+            "id": 20,
+            "offer": {
+                "id": 217,
+                "origins": [
+                    "CH"
+                ],
+                "price": "23.0000",
+                "ref": "2G81GXPEP1C3",
+                "technologies": [
+                    "HYDRO"
+                ],
+                "volume": "23"
+            },
+            "ref": "2ID7MUV9UBV0",
+            "tx_volume": "23"
+        },
+        {
+            "auction_id": 203,
+            "id": 22,
+            "offer": {
+                "id": 222,
+                "origins": [
+                    "CY"
+                ],
+                "price": "666.0000",
+                "ref": "RWM88CPRI9H1",
+                "technologies": [
+                    "HYDRO"
+                ],
+                "volume": "2"
+            },
+            "ref": "6M76LFIWM4HX",
+            "tx_volume": "7676"
+        },
+        {
+            "auction_id": 203,
+            "id": 24,
+            "offer": {
+                "id": 219,
+                "origins": [
+                    "BE"
+                ],
+                "price": "3212.0000",
+                "ref": "140MBONPELWM",
+                "technologies": [
+                    "HYDRO"
+                ],
+                "volume": "23"
+            },
+            "ref": "PRE2KN1Z38GC",
+            "tx_volume": "23"
+        },
+        {
+            "auction_id": 203,
+            "id": 26,
+            "offer": {
+                "id": 221,
+                "origins": [
+                    "BE"
+                ],
+                "price": "13232.0000",
+                "ref": "LPE2PRWUTW8Z",
+                "technologies": [
+                    "HYDRO"
+                ],
+                "volume": "12"
+            },
+            "ref": "0N6GLG0EMNCJ",
+            "tx_volume": "12"
+        },
+        {
+            "auction_id": 203,
+            "id": 28,
+            "offer": {
+                "id": 220,
+                "origins": [
+                    "BE"
+                ],
+                "price": "13232.0000",
+                "ref": "824TNR2I5MU9",
+                "technologies": [
+                    "HYDRO"
+                ],
+                "volume": "12"
+            },
+            "ref": "QEJ1DP107QD7",
+            "tx_volume": "12"
+        }
+    ]
+}
